cbd01b07f51f65599e76c330bb96460b5b0504cc
aldimiro alfredo <aldimiroalfredo@gmail.com>
2022-09-09 16:25:26 +0100

    improviment/ add validação

--- config/database.js
+++ config/database.js
@@ -5,8 +5,6 @@ const Env = use('Env')
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
-var Url = require('url-parse');
-const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
 
 module.exports = {
   /*
@@ -75,12 +73,10 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', DATABASE_URL.hostname),
-      port: Env.get('DB_PORT', DATABASE_URL.port),
-      user: Env.get('DB_USER', DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substring(1))
+      connectionString: Env.get('DATABASE_URL', ''),
+      ssl: {
+        rejectUnauthorized: true
+      }
     },
-    debug: Env.get('DB_DEBUG', false)
   }
 }
