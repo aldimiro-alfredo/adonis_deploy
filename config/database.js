1de1cf2bc777a64c29478d1fdaacaa260a8b3ffe
aldimiro alfredo <aldimiroalfredo@gmail.com>
2022-09-16 09:35:57 +0100

    improviment/ add validação conexão nas BD'Ss

--- config/database.js
+++ config/database.js
@@ -70,7 +70,10 @@ module.exports = {
   | npm i --save pg
   |
   */
-  pg: {
+  
+  /* ==== CONEXÃO VIA HEROKU ==== */
+
+  /*pg: {
     client: 'pg',
     connection: {
       connectionString: Env.get('DATABASE_URL', ''),
@@ -78,5 +81,19 @@ module.exports = {
         rejectUnauthorized: false
       }
     },
+  }*/
+
+  /* ==== CONEXÃO VIA LOCAL ==== */
+  
+  pg: {
+    client: 'pg',
+    connection: {
+      host: Env.get('DB_HOST', 'localhost'),
+      port: Env.get('DB_PORT', ''),
+      user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD', ''),
+      database: Env.get('DB_DATABASE', 'adonis')
+    },
+    debug: Env.get('DB_DEBUG', false)
   }
 }
